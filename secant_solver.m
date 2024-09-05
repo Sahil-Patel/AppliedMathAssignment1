abd693fdbf84703bd91a37821eb7ad338879db6b
Sahil-Patel <sahilpatel12321@gmail.com>
2024-09-05 00:45:52 -0400

    Update secant_solver.m

--- secant_solver.m
+++ secant_solver.m
@@ -11,15 +11,15 @@ while (abs(x_left - x_right) > Athresh && abs(fun(y_right)) > Bthresh)
 
     slope = (y_left - y_right) / (x_left - x_right);
 
-    %% x_0 is the new x_left after y_left is set to zero at the x-axis
+    % x_0 is the new x_left after y_left is set to zero at the x-axis
     x_0 = x_right - y_right / slope;
 
-   %% checking for same x_left and x_right
+   % checking for same x_left and x_right
    if slope == 0 
 
        error("same x");
    end
-   %% if x_left and x_right have opposite signs then we shift left otherwise shift right
+   % if x_left and x_right have opposite signs then we shift left otherwise shift right
    if fun(x_left) * fun(x_right) > 0
         x_left = x_0;
         y_left = fun(x_left);
